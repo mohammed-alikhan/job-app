423b8588dc4c838370829ac869b1803bf249b912
mohammed-alikhan <cntc.mak@gmail.com>
2021-06-14 18:50:20 +0530

    Updates

---
+++ precache-manifest.b7cda7a98f4eff52db773e65d2415d0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "555e828d29480f10d68ce5799776992d",
+    "url": "/job-app/index.html"
+  },
+  {
+    "revision": "473b4d018fe614e40cb4",
+    "url": "/job-app/static/css/main.fc409807.chunk.css"
+  },
+  {
+    "revision": "934f683ac2896d32a4d2",
+    "url": "/job-app/static/js/2.ab1ff8eb.chunk.js"
+  },
+  {
+    "revision": "5d2fccf369c8f06b0d0615a47be0c6de",
+    "url": "/job-app/static/js/2.ab1ff8eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "473b4d018fe614e40cb4",
+    "url": "/job-app/static/js/main.88af3f65.chunk.js"
+  },
+  {
+    "revision": "f4fab765c755612854a7",
+    "url": "/job-app/static/js/runtime-main.5b35aa3f.js"
+  }
+]);
